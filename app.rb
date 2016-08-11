921f3a647fe432c5f5d4313c5094149d05e8300c
Troy Chryssos <github email address>
2016-08-11 14:01:21 -0400

    Wrote ERB files that use ruby and HTML to output dynamic data to a webpage.

--- app.rb
+++ app.rb
@@ -1,8 +1,22 @@
 class App < Sinatra::Base
 
+	configure do
+		set :views, "views"
+	end
+
 	get '/' do
 		erb :index
 	end
 
+	get '/hello' do
+		erb :hello
+	end
 
+	get '/goodbye' do
+		erb :goodbye
+	end
+
+	get '/date' do
+		erb :date
+	end
 end
